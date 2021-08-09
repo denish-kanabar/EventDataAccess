6d3ee2f64a67ddc60d464a51da2106d3eee54ba2
Denish <53476373+denish-kanabar@users.noreply.github.com>
2021-08-09 22:41:29 +0530

    Data Model library

---
+++ EventDataAccess.Models/Batch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventDataAccess.Models
+{
+    /// <summary>
+    /// Model for Batch detail, for storing random num and multiply with random number
+    /// </summary>
+    public class Batch
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public int OperationId { get; set; }
+        public int BatchId { get; set; }
+        public int ItemPosition { get; set; }
+        public int GeneratedNum { get; set; }
+        public DateTime GeneratedTime { get; set; }
+        public int MultiplierNum { get; set; }
+        public int MultipliedNum { get; set; }
+        public DateTime MultipliedTime { get; set; }
+    }
+}
---
+++ EventDataAccess.Models/Enum.cs
@@ -0,0 +1,11 @@
+﻿namespace EventDataAccess.Models
+{
+    /// <summary>
+    /// Enum for two type of work 
+    /// </summary>
+    public enum WorkType
+    {
+        GENERATE,
+        MULTIPLY
+    }
+}
