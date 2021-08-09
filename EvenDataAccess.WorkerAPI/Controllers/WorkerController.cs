bc3707ca207f417910179e548c32d36def68fe7c
Denish <53476373+denish-kanabar@users.noreply.github.com>
2021-08-09 22:40:28 +0530

    Worker API

---
+++ EvenDataAccess.WorkerAPI/Controllers/WorkerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using EventDataAccess.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace EvenDataAccess.WorkerAPI.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    [ApiController]
+    public class WorkerController
+    {
+        private readonly ILogger<WorkerController> _logger;
+        public WorkerController(ILogger<WorkerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("Worker")]
+        public List<Batch> Worker(WorkerRequest workerRequest)
+        {
+           // _logger.LogInformation("Received request to provide generated & multiply numbers detail store in inMemory DB.");
+            WorkerStrategy workerStrategy = new();
+            var result = workerStrategy.WorkExecutor(workerRequest.Batches, workerRequest.RequestWorkType).ToList();
+            return result;
+        }
+    }
+}
